aeefd0949c166e7cd91f28118a1e2f11fd030c3f
jameshornung <hornung.james@gmail.com>
2017-06-28 13:49:56 -0400

    adding files

---
+++ public/app.js
@@ -0,0 +1,144 @@
+//++++++++++++++++++++
+//FUNCTIONS
+//++++++++++++++++++++
+
+
+// Function to retrieve and display all prospects.  First it empties the results div, the prepends the new prospect.
+// function getResults() {
+
+//   $("#results").empty();
+
+//   $.getJSON("/all", function(data) {
+//     // console.log("data= ", data);
+
+//     for (var i = 0; i < data.length; i++) {
+//       $("#results").prepend("<p class='dataentry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
+//       data[i]._id + ">" + data[i].firstName + " " + data[i].lastName + " " + data[i].university + " " + "</span><span class=deleter>Delete</span><span class=update>Edit</span></p>");
+//     }
+//   });
+// }
+
+//++++++++++++++++++++
+// EVENTS TO RUN WHEN SCRIPT LOADS
+//++++++++++++++++++++
+
+// getResults();
+
+//++++++++++++++++++++
+// CLICK EVENTS
+//++++++++++++++++++++
+
+$(document).on("click", "#all", function(){
+  $("#results").empty();
+
+  $.getJSON("/all", function(data) {
+    // console.log("data= ", data);
+
+    for (var i = 0; i < data.length; i++) {
+      $("#results").prepend("<p class='dataentry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
+      data[i]._id + ">" + data[i].firstName + " " + data[i].lastName + " " + data[i].university + " " + data[i].status + " " + "</span><span class=deleter>Delete</span><span class=update>Edit</span></p>");
+    }
+  })
+});
+
+//Click add-prospect button (submit entry form)
+$(document).on("click", "#addProspect", function() {
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: "/submit",
+    data: {
+      firstName: $("#firstName").val(),
+      lastName: $("#lastName").val(),
+      university: $("#university").val(),
+      status: $("#status").val(),
+      created: Date.now()
+    }
+  })
+  .done(function(data) {
+    
+    $("#results").prepend("<p class='dataentry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
+      data._id + ">" + data.firstName + " " + data.lastName + " " + data.university + " " + data.status + " " + "</span><span class=deleter updateProspect>Delete</span><span class=update updateProspect>Edit</span></p>");
+
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#university").val("");
+    $("#status").val("");
+  });
+});
+
+// Click "Delete" (delete a prospect)
+$(document).on("click", ".deleter", function() {
+  // Save the p tag that encloses the button
+  var selected = $(this).parent();
+  // Make an AJAX GET request to delete the specific prospect
+  // this uses the data-id of the p-tag, which is linked to the specific prospect
+  $.ajax({
+    type: "GET",
+    url: "/delete/" + selected.attr("data-id"),
+
+    // On successful call
+    success: function(response) {
+      // Remove the p-tag from the DOM
+      selected.remove();
+      // Clear the propsect info
+      $("#firstName").val("");
+      $("#lastName").val("");
+      $("#university").val("");
+      $("#status").val("");
+    }
+  });
+});
+
+// Click "Edit" (update a prospect)
+$(document).on("click", ".update", function() {
+  // Save the p tag that encloses the button
+  var selected = $(this).parent();
+  // Make an ajax call to find the prospect
+  // This uses the data-id of the p-tag, which is linked to the specific prospect
+  $.ajax({
+    type: "GET",
+    url: "/find/" + selected.attr("data-id"),
+    success: function(data) {
+      // Fill the inputs on the update form with the data that the ajax call collected
+      $("#updateFirstName").val(data.firstName);
+      $("#updateLastName").val(data.lastName);
+      $("#updateUniversity").val(data.university);
+      $("#updateStatus").val(data.status);
+      // Add a submit button to the form
+      $("#updateButtonHolder").html("<button class='submitButton' id='updater' data-id='" + data._id + "'>Submit</button>");
+    }
+  });
+});
+
+// Click "Update" button (submit update form)
+$(document).on("click", "#updater", function() {
+  // Save the selected element
+  var selected = $(this);
+  // console.log("this ", this);
+  $.ajax({
+    type: "POST",
+    url: "/update/" + selected.attr("data-id"),
+    dataType: "json",
+    data: {
+      firstName: $("#updateFirstName").val(),
+      lastName: $("#updateLastName").val(),
+      university: $("#updateUniversity").val(),
+      status: $("#updateStatus").val()
+    },
+    // On successful call
+    success: function(data) {
+      // Clear the inputs
+      $("#updateFirstName").val("");
+      $("#updateLastName").val("");
+      $("updateUniversity").val("");
+
+      //remove the update button
+      $("#updateButtonHolder").empty();
+
+      //empty the results div
+      $("#results").empty();
+    }
+  });
+});
+
---
+++ server.js
@@ -0,0 +1,141 @@
+//++++++++++++++++++++
+//DEPENDENCIES
+//++++++++++++++++++++
+
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var app = express();
+
+//++++++++++++++++++++
+//CONFIGURE THE APP
+//++++++++++++++++++++
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
+
+//++++++++++++++++++++
+//DATABASE CONFIGURATION
+//++++++++++++++++++++
+
+var databaseUrl = "trtlJR";
+var collections = ["prospects"];
+
+// Hook mongojs config to db variable
+var db = mongojs(databaseUrl, collections);
+
+// Log any mongojs errors to console
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+//++++++++++++++++++++
+//ROUTES
+//++++++++++++++++++++
+
+//-----
+//Get Requests
+//-----
+
+app.get("/", function(req, res) {
+  res.send(index.html);
+});
+
+
+app.get("/all", function(req, res) {
+  db.prospects.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+
+app.get("/delete/:id", function(req, res) {
+  db.prospects.remove({
+    "_id": mongojs.ObjectID(req.params.id)
+  }, function(error, removed) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    else {
+      console.log(removed);
+      res.send(removed);
+    }
+  });
+});
+
+
+app.get("/find/:id", function(req, res) {
+  db.prospects.findOne({
+    "_id": mongojs.ObjectId(req.params.id)
+  }, function(error, found) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    else {
+      console.log(found);
+      res.send(found);
+    }
+  });
+});
+
+
+//-----
+//Post Requests
+//-----
+
+app.post("/submit", function(req, res) {
+  db.prospects.insert(req.body, function(error, saved) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.send(saved);
+    }
+  });
+});
+
+
+app.post("/update/:id", function(req, res) {
+  db.prospects.update({
+    "_id": mongojs.ObjectId(req.params.id)
+  }, {
+    $set: {
+      "firstName": req.body.firstName,
+      "lastName": req.body.lastName,
+      "university": req.body.university,
+      "status": req.body.status,
+      "modified": Date.now()
+    }
+  }, function(error, edited) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    else {
+      console.log(edited);
+      res.send(edited);
+    }
+  });
+});
+
+
+//++++++++++++++++++++
+//LISTEN ON PORT 3000
+//++++++++++++++++++++
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
